13a521440dd826c006d4074cd56a2457e1b05bb3
Flier Lu <flier.lu@gmail.com>
2011-01-31 14:40:12 +0800

    add facebook setting

---
+++ doubanned/facebook.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+FACEBOOK_APP_NAME = 'doubanned'
+FACEBOOK_APP_ID = '<app id>'
+FACEBOOK_APP_KEY = '<app key>'
+FACEBOOK_APP_SECRET = '<app secret>'
+FACEBOOK_APP_CANVAS_PAGE = 'http://apps.facebook.com/doubanned/'
+FACEBOOK_APP_CANVAS_URL = 'http://doubanned.appspot.com/facebook/'
